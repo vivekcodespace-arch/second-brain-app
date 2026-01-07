d44646218948e2407a2d52cacfc6f2127bff80ef
Vivek Sharma <vivek.codespace@gmail.com>
2026-01-07 13:57:50 +0530

    made update shareLink endpoint

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,15 @@ import { JWT_PASS } from "./config.js";
 import { useMiddleware } from "./middleware.js";
 import mongoose from "mongoose";
 
+
+
 const app = express();
 app.use(express.json());
 
+//function for generating a link
+function generateLink(){
+    return Math.random().toString(36).substring(2,12)
+}
 
 app.post("/api/v1/signup", async (req, res) => {
     const username = req.body.usernmae;
@@ -95,14 +101,47 @@ app.delete("/api/v1",useMiddleware, async (req, res) => {
     })
 })
 
-app.post("/api/v1/brain/share", (req, res) => {
-
+app.post("/api/v1/brain/share",useMiddleware, async (req, res) => {
+    const {share} = req.body;
+    if(share){
+        const link = generateLink()
+        //same link will be stored in the db
+        await UserModel.updateOne(
+            {_id:req.userId},
+            {share: link}
+        )
+        return res.json({
+            shareLink: `/api/v1/brain/${link}`
+        })
+    }
+    else{
+        await UserModel.updateOne(
+            {_id : req.userId},
+            {$unset: {share: ""}}
+        )
+        return res.json({
+            message : "Sharing disabled."
+        })
+    }
 })
 
-app.get("/api/v1/brain/:shareLink", (req, res) => {
+app.get("/api/v1/brain/:shareLink", async (req, res) => {
+    //no authentication is required at this endpoint
+    const shareLink = req.params.shareLink
 
+    const user = await UserModel.findOne({share : shareLink})
+
+    if(!user){
+        return res.status(404).json({message:"Invalid or expored token."})
+    }
+
+    const content = await ContentModel.find({
+        userId : user._id
+    })
+
+    return res.json({
+        username: user.username,
+        content
+    })
 })
 
-app.listen(3000, () => {
-    console.log(`Server is running on port:3000`);
-})
